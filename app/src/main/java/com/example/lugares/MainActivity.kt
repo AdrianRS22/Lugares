1f114c12fd5add547989410e522f65295fb4b799
Adrian Rojas Sanchez <arojas@sofnomic.com>
2022-08-14 20:57:00 -0600

    Implementacion de autenticacion con Google Exitosa y arreglando los siguientes errores.
    1 - A la hora de obtener el certificado SHA-1, se debe de hacer en el cmd y no en el power shell ya que no reconoce la variable
    de entorno de %USERPROFILE% y al ejecutar el cmdlet da una excepcion en java porque interpreta %USERPROFILE% literalmente.
    
    2 - No se tiene que agregar el default_web_client_id en el xml de strings, da un error que los recursos estan duplicados, al parecer
    ya lo agrega cuando se conecta el sdk con el asistente de Firebase.

--- app/src/main/java/com/example/lugares/MainActivity.kt
+++ app/src/main/java/com/example/lugares/MainActivity.kt
@@ -6,15 +6,25 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.example.lugares.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val RC_SIGN_IN  = 9001
+    }
+
     private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityMainBinding
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +32,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         FirebaseApp.initializeApp(this)
         auth = Firebase.auth
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        binding.btGoogleLogin.setOnClickListener {  googleSignIn() }
 
         //Se define el metodo para el login
         binding.btLogin.setOnClickListener{
@@ -70,6 +87,37 @@ class MainActivity : AppCompatActivity() {
                     actualiza(null)
                 }
             }
+    }
+
+    private fun googleSignIn(){
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String){
+        val credentiales = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credentiales)
+            .addOnCompleteListener(this){ task ->
+                if(task.isSuccessful){
+                    val usuario = auth.currentUser
+                    actualiza(usuario)
+                }else{
+                    actualiza(null)
+                }
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == RC_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account.idToken!!)
+            }catch (e: ApiException){
+
+            }
+        }
     }
 
     private fun actualiza(user: FirebaseUser?){
